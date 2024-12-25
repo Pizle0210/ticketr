bec810e9639d2e8aca1125c8a379d75c763b7779
Pizle0210 <fajobiadedoyin@hotmail.com>
2024-12-25 23:55:54 +0100

    add spinner

---
+++ src/components/spinner.tsx
@@ -0,0 +1,7 @@
+export default function Spinner() {
+  return (
+    <div className="flex items-center justify-center min-h-[200px]">
+        <div className="w-12 h-12 border-primary border-t-2 border-b-2 rounded-full animate-spin"></div>
+    </div>
+  )
+}
